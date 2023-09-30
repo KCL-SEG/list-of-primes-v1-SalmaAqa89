35a50cb0cd6babd3ff0bc060517ebb23b48e1099
Salma Aqarrout <salma.aqarrout@kcl.ac.uk>
2023-09-30 04:09:43 +0100

    complete primes assignment

--- primes.py
+++ primes.py
@@ -3,4 +3,15 @@
 
 def primes(number_of_primes):
     list = []
+    i = 2
+    while len(list) < number_of_primes:
+        isPrime = True
+        for j in range (2, int(i**0.5) + 1):
+            if i%j == 0:
+                isPrime = False
+        if isPrime:
+            list.append(i)
+        i += 1
     return list
+
+print(primes(10))
